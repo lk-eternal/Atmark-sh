08611500c4e81ad2141288e7a7894180a34952b7
arc <arkceajin@gmail.com>
2019-05-10 17:56:07 +0900

    Update atmark-dist-sd-write.sh

--- atmark-dist-sd-write.sh
+++ atmark-dist-sd-write.sh
@@ -2,9 +2,14 @@
 # IMG_DIR="/opt/armadillo-work/atmark-dist-20180330/images"
 SD_DEV=/dev/$1
 
-if [ ! -f ${SD_DEV} ]; then
+if [ ! -e ${SD_DEV} ]; then
     echo "Error: can't found sd card device."
     exit
+fi
+
+if [ -d sd ]; then
+    sudo umount sd
+    rmdir sd
 fi
 
 mkdir sd
@@ -12,6 +17,7 @@ sudo mount -t vfat ${SD_DEV}1 sd
 sudo cp loader-armadillo840-mmcsd-*.bin sd/sdboot.bin
 sudo umount sd
 rmdir sd
+echo "loader done."
 
 mkdir romfs
 sudo mount -o loop romfs*.img romfs
@@ -24,10 +30,13 @@ sed -i "s/ram0/mmcblk0p2/" sd/etc/fstab
 sed -i "s/ext2/ext3/" sd/etc/fstab
 sudo umount sd
 rmdir sd
+echo "romfs done."
 
 mkdir sd
 sudo mount -t ext3 ${SD_DEV}2 sd
 sudo mkdir -p sd/boot
 sudo cp linux*.bin.gz sd/boot/Image.bin.gz
-sudo umount sd
+sleep 5
+sudo umount -f sd
 rmdir sd
+echo "linux done."
