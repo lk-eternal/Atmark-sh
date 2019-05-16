8bdf33113195d989e12a6dc2ddc91015fa2f1d2b
arc <arkceajin@gmail.com>
2019-05-16 09:30:16 +0900

    Update atmark-dist-sd-write.sh

--- atmark-dist-sd-write.sh
+++ atmark-dist-sd-write.sh
@@ -39,7 +39,7 @@ echo "romfs done."
 mkdir sd
 sudo mount -t ext3 ${SD_DEV}2 sd
 sudo mkdir -p sd/boot
-sudo cp linux-a840*.bin.gz sd/boot/Image.bin.gz
+sudo cp linux.bin.gz sd/boot/Image.bin.gz
 sleep 5
 sudo umount -f sd
 rmdir sd
