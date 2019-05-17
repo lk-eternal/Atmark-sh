ca538f93ae90fb11e13d13074c62b35a18d3b3d9
arc <arkceajin@gmail.com>
2019-05-17 18:09:52 +0900

    Update atmark-dist-build.sh

--- atmark-dist-build.sh
+++ atmark-dist-build.sh
@@ -109,9 +109,9 @@ sed -i "/#include <sys\/mman.h>/a\#include <sys\/resource.h>"  ${DIST_SRC}/user/
 sed -i "/^\techo 'static/d"                                    ${DIST_SRC}/user/gstreamer/gstreamer1.0/gstreamer1.0-1.0.8/gst/parse/Makefile.am
 sed -i 's?/$(CROSS_COMPILE:-=)/lib?/lib/$(CROSS_COMPILE:-=)?'  ${DIST_SRC}/user/qt5/Makefile
 
-ARMADILLO840_MAKEFILE = ${DIST_SRC}/vendors/AtmarkTechno/Armadillo-840/Makefile
-BEFORE_MAKE_CMD = '$(ROOTDIR)/atmark-dist-before-make.sh $(ROMFSIMG)'
-BEFORE_MAKE_PROCESS = '/\t$(call do_depmod)/a\\tbash '
+ARMADILLO840_MAKEFILE=${DIST_SRC}/vendors/AtmarkTechno/Armadillo-840/Makefile
+BEFORE_MAKE_CMD='$(ROOTDIR)/atmark-dist-before-make.sh $(ROMFSIMG)'
+BEFORE_MAKE_PROCESS='/\t$(call do_depmod)/a\\tbash '
 if grep -q "${BEFORE_MAKE_CMD}" ${ARMADILLO840_MAKEFILE}; then
   echo 'Before-make shell added into armadillo-840 makefile.'
 else
