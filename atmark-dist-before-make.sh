df8a94b5578e91113a7ddd1f323f46613a373d30
arc <arkceajin@gmail.com>
2019-05-20 13:30:51 +0900

    Update atmark-dist-before-make.sh

--- atmark-dist-before-make.sh
+++ atmark-dist-before-make.sh
@@ -1,6 +1,7 @@
-ROMFSDIR = $1
-OPENCV_DIR = "/usr/opencv-*/lib"
-MACHINE_ID = ${ROMFSDIR}/etc/machine-id
+ROMFSDIR=$1
+QT_DIR="/opt/Qt/Qt5.3.2-arm/"
+OPENCV_DIR="/usr/opencv-*/lib"
+MACHINE_ID=${ROMFSDIR}/etc/machine-id
 QT_QPA="QT_QPA_EGLFS_DISPLAY=1
 export QT_QPA_EGLFS_DISPLAY
 QT_QPA_PLATFORM=linuxfb:fb=/dev/fb1
@@ -15,6 +16,9 @@ sudo rm -f ${ROMFSDIR}/usr/lib/*.so.*
 sudo cp -a /usr/lib/arm-linux-gnueabihf/*.so*             ${ROMFSDIR}/lib
 sudo cp -a /usr/lib/arm-linux-gnueabihf/pulseaudio/*.so*  ${ROMFSDIR}/lib
 sudo cp -a /lib/arm-linux-gnueabihf/*.so*                 ${ROMFSDIR}/lib
+sudo cp -a ${QT_DIR}/lib/*.so*                            ${ROMFSDIR}/lib
+sudo cp -rf ${QT_DIR}/plugins                             ${ROMFSDIR}/lib/qt5
+sudo cp -rf ${QT_DIR}/qml                                 ${ROMFSDIR}/lib/qt5
 
 if [ -d ${OPENCV_DIR} ]; then
   sudo cp -a ${OPENCV_DIR}/*.so*              ${ROMFSDIR}/lib
