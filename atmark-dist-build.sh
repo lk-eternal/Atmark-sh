799c996875135ebea0c7e8bccd2b30f079a09692
arc <arkceajin@gmail.com>
2019-05-22 15:40:12 +0900

    Update atmark-dist-build.sh

--- atmark-dist-build.sh
+++ atmark-dist-build.sh
@@ -36,34 +36,34 @@ fi
 
 # install libs
 #sudo apt-get install virtualbox-guest-dkms virtualbox-guest-x11 linux-headers-$(uname -r) -y
-sudo apt-get update
+#sudo apt-get update
 #sudo apt-get upgrade -y
-sudo aptitude install gstreamer1.0-plugins-bad:armhf -y
-sudo apt-get install libasound2-dev:armhf -y
-sudo apt-get install libexpat1-dev:armhf -y
-sudo apt-get install libdaemon-dev:armhf -y
-sudo apt-get install bison -y
-sudo apt-get install flex -y
-sudo apt-get install libglib2.0-dev:armhf -y
-sudo apt-get install libjpeg-dev:armhf -y
-sudo apt-get install zlib1g-dev:armhf -y
-sudo apt-get install liblzo2-dev:armhf -y
-sudo apt-get install libssl-dev:armhf -y
-sudo apt-get install gstreamer1.0-alsa:armhf -y
-sudo apt-get install gstreamer1.0-libav:armhf -y
-sudo apt-get install gstreamer1.0-plugins-good:armhf -y
-sudo apt-get install qt5-qmake:armhf -y
-sudo apt-get install qt5-default:armhf -y
-sudo apt-get install qtbase5-private-dev:armhf -y
-sudo apt-get install qtdeclarative5-dev:armhf -y
-sudo apt-get install qttools5-dev:armhf -y
-sudo apt-get install qtxmlpatterns5-dev-tools:armhf -y
-sudo apt-get install qmlscene:armhf -y
-sudo apt-get install qml-module-qtquick2:armhf -y
-sudo apt-get install qml-module-qttest:armhf -y
-sudo apt-get install qml-module-qtquick-window2:armhf -y
-sudo apt-get install qml-module-qtmultimedia:armhf -y
-sudo apt-get install genext2fs -y
+#sudo aptitude install gstreamer1.0-plugins-bad:armhf -y
+#sudo apt-get install libasound2-dev:armhf -y
+#sudo apt-get install libexpat1-dev:armhf -y
+#sudo apt-get install libdaemon-dev:armhf -y
+#sudo apt-get install bison -y
+#sudo apt-get install flex -y
+#sudo apt-get install libglib2.0-dev:armhf -y
+#sudo apt-get install libjpeg-dev:armhf -y
+#sudo apt-get install zlib1g-dev:armhf -y
+#sudo apt-get install liblzo2-dev:armhf -y
+#sudo apt-get install libssl-dev:armhf -y
+#sudo apt-get install gstreamer1.0-alsa:armhf -y
+#sudo apt-get install gstreamer1.0-libav:armhf -y
+#sudo apt-get install gstreamer1.0-plugins-good:armhf -y
+#sudo apt-get install qt5-qmake:armhf -y
+#sudo apt-get install qt5-default:armhf -y
+#sudo apt-get install qtbase5-private-dev:armhf -y
+#sudo apt-get install qtdeclarative5-dev:armhf -y
+#sudo apt-get install qttools5-dev:armhf -y
+#sudo apt-get install qtxmlpatterns5-dev-tools:armhf -y
+#sudo apt-get install qmlscene:armhf -y
+#sudo apt-get install qml-module-qtquick2:armhf -y
+#sudo apt-get install qml-module-qttest:armhf -y
+#sudo apt-get install qml-module-qtquick-window2:armhf -y
+#sudo apt-get install qml-module-qtmultimedia:armhf -y
+#sudo apt-get install genext2fs -y
 
 # fix qmake link
 sudo rm /usr/bin/qmake
