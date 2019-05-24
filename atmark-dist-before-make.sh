1865776a4b41a3994acd6e6e358973e3d4c85ef3
arc <arkceajin@gmail.com>
2019-05-24 10:39:40 +0900

    Update atmark-dist-before-make.sh

--- atmark-dist-before-make.sh
+++ atmark-dist-before-make.sh
@@ -11,9 +11,11 @@ sudo cp -a /lib/arm-linux-gnueabihf/*.so*                 ${ROMFSDIR}/lib
 sudo cp -r /usr/lib/arm-linux-gnueabihf/gstreamer0.10     ${ROMFSDIR}/lib
 sudo cp -r /usr/lib/arm-linux-gnueabihf/gstreamer-0.10    ${ROMFSDIR}/lib
 if [ -d ${QT_DIR} ]; then
-  sudo cp -a ${QT_DIR}/lib/*.so*                          ${ROMFSDIR}/lib
-  sudo cp -rf ${QT_DIR}/plugins                           ${ROMFSDIR}/lib/qt5
-  sudo cp -rf ${QT_DIR}/qml                               ${ROMFSDIR}/lib/qt5
+  #sudo cp -a ${QT_DIR}/lib/*.so*     ${ROMFSDIR}/lib
+  #sudo cp -rf ${QT_DIR}/plugins      ${ROMFSDIR}/lib/qt5
+  #sudo cp -rf ${QT_DIR}/qml          ${ROMFSDIR}/lib/qt5
+  sudo mkdir ${ROMFSDIR}/opt/Qt
+  sudo cp -rf ${QT_DIR}/qml           ${ROMFSDIR}/opt/Qt
 fi
 
 if [ -d ${OPENCV_DIR} ]; then
@@ -30,10 +32,12 @@ if grep -q "QPA" ${ROMFSDIR}/etc/profile; then
     echo "QPA already added."
 else
     PROFILE=${ROMFSDIR}/etc/profile
-    sudo echo "export QT_QPA_PLATFORM=eglfs" >> $PROFILE
-    sudo echo "export QT_QPA_EGLFS_DISPLAY=1" >> $PROFILE
-    sudo echo "export QT_QPA_EGLFS_WIDTH=800" >> $PROFILE
-    sudo echo "export QT_QPA_EGLFS_HEIGHT=480" >> $PROFILE
-    sudo echo "export QT_QPA_EGLFS_FB=/dev/fb1" >> $PROFILE
+# EGLFS not work at armdillo840!!!
+#     sudo echo "export QT_QPA_PLATFORM=eglfs" >> $PROFILE
+#     sudo echo "export QT_QPA_EGLFS_DISPLAY=1" >> $PROFILE
+#     sudo echo "export QT_QPA_EGLFS_WIDTH=800" >> $PROFILE
+#     sudo echo "export QT_QPA_EGLFS_HEIGHT=480" >> $PROFILE
+#     sudo echo "export QT_QPA_EGLFS_FB=/dev/fb1" >> $PROFILE
+    sudo echo "export QT_QPA_PLATFORM=linuxfb:fb=/dev/fb1" >> $PROFILE
     echo "QPA add into profile."
 fi
